dd2d80dd09b58df424f9192df53df0fe18ff96cd
Abdekader Esaam <118766308+Eouronymous9990@users.noreply.github.com>
2024-04-30 00:27:27 +0300

    Add files via upload

---
+++ OOB.py.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import PIL.Image as Image
+import gradio as gr
+
+from ultralytics import ASSETS, YOLO
+
+model = YOLO(r"C:\Users\zbook 17 g3\Downloads\best(1).pt")
+
+
+def predict_image(img, conf_threshold, iou_threshold):
+    results = model.predict(
+        source=img,
+        conf=conf_threshold,
+        iou=iou_threshold,
+        show_labels=True,
+        show_conf=True,
+        imgsz=640,
+    )
+
+    for r in results:
+        im_array = r.plot()
+        im = Image.fromarray(im_array[..., ::-1])
+
+    return im
+
+
+iface = gr.Interface(
+    fn=predict_image,
+    inputs=[
+        gr.Image(type="pil", label="Upload Image"),
+        gr.Slider(minimum=0, maximum=1, value=0.25, label="Confidence threshold"),
+        gr.Slider(minimum=0, maximum=1, value=0.45, label="IoU threshold")
+    ],
+    outputs=gr.Image(type="pil", label="Result"),
+    title="Ultralytics Gradio",
+    description="Upload images for inference. The Ultralytics YOLOv8n model is used by default.",
+    examples=[
+        [ASSETS / "bus.jpg", 0.25, 0.45],
+        [ASSETS / "zidane.jpg", 0.25, 0.45],
+    ]
+)
+
+if __name__ == '__main__':
+    iface.launch()
+
+
+# In[ ]:
+
+
+
+
